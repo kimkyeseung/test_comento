498951f21aad0fc7999626859a6e89dfd93911ae
김계승 <>
2018-11-15 03:26:25 +0900

    get started project, set less

---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <div class="filter">
+      <button>필터</button>
+      <div class="order">
+        <input type="radio" name="order" id="orderAsc">
+        <label for="orderAsc">오름차순</label>
+        <input type="radio" name="order" id="orderDesc">
+        <label for="orderDesc">내림차순</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <div>
+      <ul>
+        <li>hi~</li>
+        <li>hi~</li>
+        <li>hi~</li>
+        <li>hi~</li>
+        <li>hi~</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data() {
+    return {
+      title: 'Hi! Vue',
+      body: 'maybe we gonna be good friends',
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+h1 {
+  font-size: 50px;
+}
+</style>
